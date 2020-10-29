14940291550a406baa57d531d5e4fd708e34bc13
paxob <paxob@DESKTOP-JTRP68P>
2020-10-28 19:44:54 -0700

    added to enemyType

--- group-project/src/starter/enemyType.java
+++ group-project/src/starter/enemyType.java
@@ -1,5 +1,14 @@
 package starter;
 
-public class enemyType {
-
+public enum enemyType {
+	RED, GREEN, BLUE;
+	
+	public String toString() {
+		switch(this) {
+			case RED: return "red";
+			case GREEN: return "green";
+			case BLUE: return "blue";
+		}
+		return "n/a";
+	}
 }
