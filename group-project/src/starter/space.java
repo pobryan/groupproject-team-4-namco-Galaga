14bc4f9131cc80a6c7377c3f19f8e49fffb4c78d
NupurSompura <n_sompura@u.pacific.edu>
2020-10-28 19:39:07 -0700

    added to the Space class by adding getters and setters

--- group-project/src/starter/space.java
+++ group-project/src/starter/space.java
@@ -1,5 +1,27 @@
 package starter;
 
 public class space {
-
+	private int x;
+	private int y;
+	
+	public space(int newX, int newY) {
+		x = newX;
+		y = newY;
+	}
+	
+	public void setX(int newX) {
+		x = newX;
+	}
+	
+	public void setY(int newY) {
+		y = newY;
+	}
+	
+	public int getX() {
+		return x;
+	}
+	
+	public int getY() {
+		return y;
+	}
 }
