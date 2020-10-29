116412c3268cd5e42dc0deb031dfa77edbf7f999
Marissa Morones <m_morones@u.pacific.edu>
2020-10-28 20:16:17 -0700

    Created getSpace(), setSpace(), enemy(), isEnemyDead(), move(), and shoot() methods

--- group-project/src/starter/enemy.java
+++ group-project/src/starter/enemy.java
@@ -3,6 +3,38 @@ package starter;
 public class enemy {
 	private enemyType eType;
 	private space start;
+	double speed;
 	
-
+	enemy(int x, int y, double speed) {
+		start.setX(x);
+		start.setY(y);
+		this.speed=speed;
+	}
+	
+	//TODO: Finish this method. This method should check if the enemy was hit.
+	public boolean isEnemyDead(){
+		return true;
+	}
+	
+	//TODO: Finish this method. It needs to use Bullet to fire at Fighter.
+	public void shoot() {
+		
+	}
+	
+	public void move(space s1, space s2) {
+		int difX=s1.getX()-s2.getX();
+		int difY=s1.getY()-s2.getY();
+		
+		start.setX(start.getX()+difX);
+		start.setY(start.getY()+difY);
+	}
+	
+	public space getSpace() {
+		return start;
+	}
+	
+	public void setSpace(int x, int y) {
+		start.setX(x);
+		start.setY(y);
+	}
 }
