696dee80b50960622471c8e66efbf9e7484c8fbc
Ryan <ryanribbel@gmail.com>
2020-10-29 00:02:15 -0700

    Made more instance variables/functions for Bullet

--- group-project/src/starter/Bullet.java
+++ group-project/src/starter/Bullet.java
@@ -1,16 +1,39 @@
 package starter;
 
+import java.awt.Color;
+
+import acm.graphics.GRect;
 import acm.program.GraphicsProgram;
 
 public class Bullet extends GraphicsProgram{
-	public static final int SPEED = 2;
+	public static final int WIDTH = 5;
+	public static final int HEIGHT = 15;
 	
 	private space start;
+	private double speed;
+	private GRect bullet;
 	
+	public GRect makeBullet(double x, double y) {
+		GRect tempBullet = new GRect(x-WIDTH/2, y-HEIGHT/2, WIDTH, HEIGHT);
+		tempBullet.setColor(Color.ORANGE);
+		tempBullet.setFilled(true);
+		return tempBullet;
+	}
+	
+	//getters
 	public space getStart() {
 		return start;
 	}
-	public void setStart(space s) {
-		start = s;
+	public double getSpeed() {
+		return speed;
+	}
+	
+	//setters
+	public void setStart(int x, int y) {
+		start.setX(x);
+		start.setY(x);
+	}
+	public void setSpeed(double speed) {
+		this.speed = speed;
 	}
 }
