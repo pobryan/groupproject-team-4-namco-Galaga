40c4b1d1bf6ab93a9d93ffc3cc8011aaf7f39b6f
Ryan <ryanribbel@gmail.com>
2020-10-28 20:14:37 -0700

    Added some instance variables to bullet along with getters and setters

--- group-project/src/starter/Bullet.java
+++ group-project/src/starter/Bullet.java
@@ -1,5 +1,16 @@
 package starter;
 
-public class Bullet {
+import acm.program.GraphicsProgram;
 
+public class Bullet extends GraphicsProgram{
+	public static final int SPEED = 2;
+	
+	private space start;
+	
+	public space getStart() {
+		return start;
+	}
+	public void setStart(space s) {
+		start = s;
+	}
 }
