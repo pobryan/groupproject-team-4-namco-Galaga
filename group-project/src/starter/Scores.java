aeace05e92cc205e0e54461a7471de8b9156f252
paxob <paxob@DESKTOP-JTRP68P>
2020-11-01 02:56:15 -0800

    Spent some time experimenting with files, added a small baseline for reading them

--- group-project/src/starter/Scores.java
+++ group-project/src/starter/Scores.java
@@ -1,5 +1,13 @@
 package starter;
 
+import java.io.*; 
+
 public class Scores {
+	public static void main(String[] args)throws Exception {
+		
+		File fileName = new File("");
+		BufferedReader br = new BufferedReader(new FileReader(fileName));
+		//still figuring out reading in and editing files in java
+	}
 
 }
