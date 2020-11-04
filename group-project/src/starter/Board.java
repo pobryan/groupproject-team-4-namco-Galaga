50ed81686df21e54799d9cebb44036375a70dad4
Vincent Torres <v_torres3@u.pacific.edu>
2020-11-04 12:05:03 -0800

    added private variables and a couple functions to kick start Board

--- group-project/src/starter/Board.java
+++ group-project/src/starter/Board.java
@@ -1,5 +1,23 @@
 package starter;
+import java.util.*;
+
+/*checks the fighter stays within the bounds of the board*/
 
 public class Board {
-
+	Fighter[][] grid;
+	private int numRows;
+	private int numCols;
+	
+	
+	public Board(int rows, int cols) {
+		numCols = cols;
+		numRows = rows;
+		grid = new Fighter[rows][cols];
+	}
+	public int getNumCols() {
+		return numCols;
+	}
+	public int getNumRows() {
+		return numRows;
+	}
 }
