40a0942bb0e3981bf2871668946fed3f51bac284
paxob <paxob@DESKTOP-JTRP68P>
2020-11-04 04:08:38 -0800

    Added base setter, getter, and display/keypressed

--- group-project/src/starter/Scores.java
+++ group-project/src/starter/Scores.java
@@ -1,13 +1,37 @@
 package starter;
 
+import java.awt.event.ActionEvent;
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
 import java.io.*; 
 
 public class Scores {
+	private Scores newScore;
+	
 	public static void main(String[] args)throws Exception {
 		
 		File fileName = new File("");
 		BufferedReader br = new BufferedReader(new FileReader(fileName));
 		//still figuring out reading in and editing files in java
 	}
+	
+	public Scores getScore() {
+		return newScore;
+	}
+	
+	public void setScore() {
+		
+	}
 
+	public void displayScores(Graphics text) {
+		text.drawString("sample text", 100, 200);
+	}
+	
+	public void keyPressed(KeyEvent e) { 
+		int key = e.getKeyCode();
+		if (key == KeyEvent.VK_ESCAPE) {
+	       
+	      }
+	}
+	
 }
