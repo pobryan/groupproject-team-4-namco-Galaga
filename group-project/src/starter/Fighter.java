5f513c6de34c83511503d58f5510d880bf9a7114
Marissa Morones <m_morones@u.pacific.edu>
2020-11-06 16:43:38 -0800

    Added beginning of methods to Fighter

--- group-project/src/starter/Fighter.java
+++ group-project/src/starter/Fighter.java
@@ -4,7 +4,24 @@ public class Fighter {
 	private space position;
 	double speed;
 	int lives;
+	Bullet shoot;
 	
+	//Getters
+	public int getLives() {
+		return lives;
+	}
+	
+	public space getPosition() {
+		return position;
+	}
+	
+	//Setters
+	public void setPosition(int x, int y) {
+		position.setX(x);
+		position.setY(y);
+	}
+	
+	//Constructor
 	Fighter(int x, int y, space p, int l){
 		position.setX(x);
 		position.setY(y);
@@ -12,11 +29,37 @@ public class Fighter {
 		lives = l;
 	}
 	
-	public space getPosition() {
-		return position;
+	//Methods
+	public void addLife() {
+		lives++;
 	}
-	public void setPosition(int x, int y) {
-		position.setX(x);
-		position.setY(y);
+	
+	public void loseLife() {
+		lives--;
 	}
+	
+	public boolean isLivesEmpty() {
+		if(lives==-1) {
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean isFighterHit() {
+		return true;
+	}
+	
+	public void shoot() {
+		
+	}
+	
+	public void moveLeft() {
+		
+	}
+	
+	public void moveRight() {
+		
+	}
+	
+	
 }
