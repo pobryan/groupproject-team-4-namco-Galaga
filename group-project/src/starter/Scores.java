77158aa44b57298b85bd5e220e6502118ff1bf84
pobryan <72995425+pobryan@users.noreply.github.com>
2023-01-30 20:58:16 -0800

    Beginning display
    Begins by reading in the value from the text file, print to show

--- group-project/src/starter/Scores.java
+++ group-project/src/starter/Scores.java
@@ -1,37 +1,25 @@
-package starter;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
-import java.awt.event.ActionEvent;
-import java.awt.Graphics;
-import java.awt.event.KeyEvent;
-import java.io.*; 
+public class ScoreT {
+  public static void main(String[] args) {
+	  
+	try {
+	    BufferedReader reader = new BufferedReader(new FileReader("highscore.txt"));
+	    String hscore;
+	    while((hscore = reader.readLine()) != null) {
+	      System.out.printf("The current high score is: ");
+	      System.out.println(hscore);
+	    }
+	    reader.close();
+	  } catch (IOException e) {
+	    e.printStackTrace();
+	  }
+  }
+}
 
-public class Scores {
-	private Scores newScore;
-	
-	public static void main(String[] args)throws Exception {
-		
-		File fileName = new File("");
-		BufferedReader br = new BufferedReader(new FileReader(fileName));
-		//still figuring out reading in and editing files in java
-	}
-	
-	public Scores getScore() {
-		return newScore;
-	}
-	
-	public void setScore() {
-		
-	}
-
-	public void displayScores(Graphics text) {
-		text.drawString("sample text", 100, 200);
-	}
-	
-	public void keyPressed(KeyEvent e) { 
-		int key = e.getKeyCode();
-		if (key == KeyEvent.VK_ESCAPE) {
-	       
-	      }
-	}
 	
 }
