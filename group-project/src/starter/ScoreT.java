8fe97b924e039697a0557f133266a99b1c0c2f0f
pobryan <72995425+pobryan@users.noreply.github.com>
2023-01-30 21:43:53 -0800

    Converting string to int
    Integer is needed for comparison

--- group-project/src/starter/ScoreT.java
+++ group-project/src/starter/ScoreT.java
@@ -6,17 +6,29 @@ import java.io.IOException;
 
 public class ScoreT {
   public static void main(String[] args) {
-	  
+	String hscore;
+	String temp = "1";
 	try {
 	    BufferedReader reader = new BufferedReader(new FileReader("highscore.txt"));
-	    String hscore;
 	    while((hscore = reader.readLine()) != null) {
 	      System.out.printf("The current high score is: ");
 	      System.out.println(hscore);
+	      temp = hscore;
 	    }
 	    reader.close();
 	  } catch (IOException e) {
 	    e.printStackTrace();
 	  }
+	
+    try{
+        int number = Integer.parseInt(temp);
+        System.out.println(number); // output = 25
+    }
+    catch (NumberFormatException ex){
+        ex.printStackTrace();
+    }
+
+
+    
   }
 }
