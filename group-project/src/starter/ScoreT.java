c7d61d8919482d8f66c96ebfa44cfe42fbc46779
pobryan <72995425+pobryan@users.noreply.github.com>
2023-01-30 22:37:38 -0800

    Updates when new score is greater

--- group-project/src/starter/ScoreT.java
+++ group-project/src/starter/ScoreT.java
@@ -6,16 +6,17 @@ import java.io.IOException;
 
 public class ScoreT {
   public static void main(String[] args) {
-	String score;
-	String temp = "1";
+	String score;		//file save variable
+	String temp = "1";	//temporary transition variables
 	int itemp = 1;
+	int newScore = 300;
 	try {
 	    BufferedReader reader = new BufferedReader(new FileReader("highscore.txt"));
-	    while((score = reader.readLine()) != null) {
-	      System.out.printf("The current high score is: ");
+	    while((score = reader.readLine()) != null) {	//reading file
+	      System.out.printf("The current high score is: ");		//printing
 	      System.out.println(score);
-	      temp = score;
-	    }
+	      temp = score;		//transition for string -> integer conversion
+	    }	
 	    reader.close();
 	  } catch (IOException e) {
 	    e.printStackTrace();
@@ -23,22 +24,23 @@ public class ScoreT {
 	
     try{
         int number = Integer.parseInt(temp);
-        System.out.println(number); // output = 25
         itemp = number;
     }
     catch (NumberFormatException ex){
         ex.printStackTrace();
     }
-    
-    try {
-        BufferedWriter writer = new BufferedWriter(new FileWriter("highscore.txt"));
-        String s=String.valueOf(itemp);
-        writer.write(s);
-        
-        writer.close();
-      } catch (IOException e) {
-        e.printStackTrace();
-      }
-    
+    if (newScore > itemp) {
+    	try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter("highscore.txt"));
+            String s=String.valueOf(newScore);
+            System.out.printf("New High Score is: ");
+            System.out.println(s);
+            writer.write(s);
+            
+            writer.close();
+          } catch (IOException e) {
+            e.printStackTrace();
+          }
+    }
   }
 }
